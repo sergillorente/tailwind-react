e14ee5937ed7bf807178e4d99b369dfcc2e96b55
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-02-11 12:10:11 +0100

    Content component finished and starting to use the @apply in order not to repeat code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
+import Content from './components/Content';
 
 function App() {
   return (
     <>
       <NavBar />
       <Hero />
+      <Content />
     </>
   );
 };
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import ImageOne from '../images/Breakfast.jpeg';
+import ImageTwo from '../images/SmilyEgg.jpeg';
 
 const Content = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="menu-card">
+                <img src={ImageOne} alt="" className="h-full rounded mb-20 shadow" />
+                <div className="center-content">
+                    <h2 className="text-2xl mb-2">Breakfast Egg</h2>
+                    <p className="mb-2">Wake up, it's time to eat! Yumi!</p>
+                    <span>$16</span>
+                </div>
+            </div>
+            <div className="menu-card">
+                <img src={ImageTwo} alt="" className="h-full rounded mb-20 shadow" />
+                <div className="cemter-content">
+                    <h2 className="text-2xl mb-2">Smily Egg</h2>
+                    <p className="mb-2">Hey! Have a great day!</p>
+                    <span>$free</span>
+                </div>
+            </div>
+        </>
     );
 };
 
