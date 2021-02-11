ad9f87b9291d328bf59b55df0b10255313c96a28
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-02-11 11:32:41 +0100

    Creation of the NavBar with a responsive design using tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-red-500">Yooo</h1>
-    </div>
+    <>
+      <NavBar />
+    </>
   );
 };
 
