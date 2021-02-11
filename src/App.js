9e18098aeeca51daf65557a882a0aab11f340610
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-02-11 13:50:28 +0100

    The basics of the App are done. There is room for further improvements in the future

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React , { useState, useEffect } from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
@@ -6,11 +7,33 @@ import Footer from './components/Footer';
 import Home from './pages/index';
 import About from './pages/about';
 import Menu from './pages/menu';
+import Dropdown from './components/Dropdown';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false)
+      }
+    }
+
+    window.addEventListener('resize', hideMenu)
+
+    return () => {
+      window.removeEventListener('resize', hideMenu);
+    }
+  })
+
   return (
     <>
-      <NavBar />
+      <NavBar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/menu" component={Menu} />
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Dropdown = () => {
+const Dropdown = ({ isOpen, toggle }) => {
     return (
-        <div>
+        <div className={
+            isOpen ? 
+            "grid grid-rows-4 text-center items-center bg-yellow-500" : 
+            'hidden'}
+            onClick={toggle}
+            >
             <Link className='p-4' to='/'>
                 Home
             </Link> 
